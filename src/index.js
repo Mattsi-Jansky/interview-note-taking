504fea598f6a9b00776d663b3d7c9baf2e89aaed
Mattsi <mattsi.jansky@codurance.com>
2021-10-16 16:44:02 +0100

    Add some temporary test data to the app for now

--- src/index.js
+++ src/index.js
@@ -3,9 +3,28 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const testCategories = [
+  { 
+    name:"Technical Depth",
+    questions: [
+      "What is the average speed of an unladen swallow?",
+      "What is your favourite color?",
+      "Do you feel lucky?"
+    ]
+  },
+  {
+    name:"Leadership",
+    questions: [
+      "What is the average speed of an unladen swallow?",
+      "What is your favourite color?",
+      "Do you feel lucky?"
+    ]
+  }
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App categories={testCategories} />
   </React.StrictMode>,
   document.getElementById('root')
 )
